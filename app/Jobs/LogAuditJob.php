6bf4593838da48370aa4cdde01347886bcf9e929
Abdlazz0 <154979182+Abdlazz0@users.noreply.github.com>
2026-05-23 10:51:38 +0700

    feat: setup supervisord with queue worker and nightwatch agent

---
+++ app/Jobs/LogAuditJob.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\ActivityLog;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class LogAuditJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Number of times the job may be attempted.
+     */
+    public int $tries = 3;
+
+    /**
+     * Number of seconds to wait before retrying the job.
+     */
+    public int $backoff = 5;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(protected array $data) {}
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        ActivityLog::create($this->data);
+    }
+}
